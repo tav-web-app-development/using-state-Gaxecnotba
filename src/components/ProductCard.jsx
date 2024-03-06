41b079044e4184e20008802e5f1c982352a2d586
Sebastian Rojas <sebasleprodevj12@gmail.com>
2024-03-05 19:20:32 -0500

    Assignment 2230911

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,25 +1,42 @@
+import { useState } from "react";
 export default function ProductCard({ product }) {
-  let currentImageIndex = 0;
-  let itemsInCart = 0;
+  const [currentImageIndex, setImage] = useState(0);
+  const [itemsInCart, setItems] = useState(1);
+  const [showDescription, setShow] = useState(false);
 
   const handleAddToCartClick = () => {
-    itemsInCart++;
+    setItems(itemsInCart + 1);
     alert(`you added ${itemsInCart}`);
   };
+  // function nextClick() {
+  //   setImage(currentImageIndex + 1);
+  // }
+
   return (
     <>
       <div id="image-carousel">
-        <img
-          src={product.imageUrls[currentImageIndex] + " " + product.name}
-          alt={product.name}
-        />
-        <button>Next</button>
-        <button>Previous</button>
+        <img src={product.imageUrls[currentImageIndex]} alt={product.name} />
+        <button
+          onClick={() => setImage(currentImageIndex + 1)}
+          disabled={currentImageIndex === product.imageUrls.length - 1}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => setImage(currentImageIndex - 1)}
+          disabled={currentImageIndex === 0}
+        >
+          Previous
+        </button>
       </div>
 
       <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <button>Show Description</button>
+
+      {showDescription && <p>{product.description}</p>}
+      <button onClick={() => setShow(!showDescription)}>
+        {showDescription ? "Hide Descriptions" : "Show Description"}
+      </button>
+
       <div className="price">${product.price}</div>
 
       <button onClick={handleAddToCartClick}>Add to Cart</button>
